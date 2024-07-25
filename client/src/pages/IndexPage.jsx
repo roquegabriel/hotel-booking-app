066f7f438afa99f1cfdb0d44d53b5f2feb70d7a3
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-07-25 16:11:52 -0400

    Clean code

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { Header } from "../components/Header";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Image from "../components/Image";
